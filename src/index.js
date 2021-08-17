9714c90a3be4275421137bdd57babc13fe7d10b6
AsdobsA <alexsamakar@gmail.com>
2021-08-18 00:47:06 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let number, result = 0;
+    if (n < 0) {
+        n=n*(-1);
+    }
+    while(n){             
+        result = result * 10 + n%10;
+        n = Math.floor(n/10);                     
+    }  
+    return result;
 }
+
+
